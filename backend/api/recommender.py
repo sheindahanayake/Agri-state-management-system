9edcca974db9342d178e0e903bd9ba7fcd28e6e6
IsharaEkanayaka <e20094@eng.pdn.ac.lk>
2025-01-25 16:44:15 +0530

    remove unwanted libraries

--- backend/api/recommender.py
+++ backend/api/recommender.py
@@ -1,16 +1,3 @@
-import numpy as np
-import pandas as pd
-import warnings
-import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import confusion_matrix,accuracy_score
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import accuracy_score, classification_report
 import pickle
 import os
 
